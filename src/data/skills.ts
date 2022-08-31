ff431662d99365c22433cedfc16615ef08084174
soumya-99 <soumyadeepmondal1234@gmial.com>
2022-09-01 01:14:08 +0530

    Skills data added

---
+++ src/data/skills.ts
@@ -0,0 +1,104 @@
+export const WEB_SKILLS: { name: string, id: string, color: string, progress: number }[] = [
+    {
+        name: "React TS/JS + Vite",
+        id: "pg1",
+        color: "primary",
+        progress: 80,
+    },
+    {
+        name: "Django",
+        id: "pg2",
+        color: "primary",
+        progress: 70,
+    },
+    {
+        name: "Firebase",
+        id: "pg3",
+        color: "primary",
+        progress: 60,
+    },
+    {
+        name: "React Native (TS/JS)",
+        id: "pg4",
+        color: "primary",
+        progress: 70,
+    },
+]
+
+export const PROGRAMMING_LANGS: { name: string, id: string, color: string, progress: number }[] = [
+    {
+        name: "TypeScript",
+        id: "pg5",
+        color: "secondary",
+        progress: 80,
+    },
+    {
+        name: "JavaScript",
+        id: "pg6",
+        color: "secondary",
+        progress: 80,
+    },
+    {
+        name: "Python (3.10.2)",
+        id: "pg7",
+        color: "secondary",
+        progress: 70,
+    },
+    {
+        name: "C",
+        id: "pg8",
+        color: "secondary",
+        progress: 70,
+    },
+    {
+        name: "Java",
+        id: "pg9",
+        color: "secondary",
+        progress: 75,
+    },
+    {
+        name: "HTML5",
+        id: "pg10",
+        color: "secondary",
+        progress: 95,
+    },
+    {
+        name: "CSS3",
+        id: "pg11",
+        color: "secondary",
+        progress: 90,
+    },
+]
+
+export const TECH_KNOWN: { name: string, id: string, color: string, progress: number }[] = [
+    {
+        name: "Git and GitHub",
+        id: "pg12",
+        color: "tertiary",
+        progress: 85,
+    },
+    {
+        name: "Material UI",
+        id: "pg13",
+        color: "tertiary",
+        progress: 85,
+    },
+    {
+        name: "Material You (Material Design 3)",
+        id: "pg14",
+        color: "tertiary",
+        progress: 75,
+    },
+    {
+        name: "Operating System",
+        id: "pg15",
+        color: "tertiary",
+        progress: 60,
+    },
+    {
+        name: "Unix and Linux",
+        id: "pg16",
+        color: "tertiary",
+        progress: 60,
+    },
+]
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -3,6 +3,8 @@ import ProgressBar from "../components/ProgressBar"
 import Ball from "../animation/Ball"
 import Square from "../animation/Square"
 
+import { WEB_SKILLS, PROGRAMMING_LANGS, TECH_KNOWN } from "../data/skills"
+
 function Skills() {
 	return (
 		<>
@@ -22,81 +24,74 @@ function Skills() {
 						{"<>"}Web Skills{"</>"}
 					</h4>
 					<div className="skills">
-						<p>React TS/JS + Vite</p>
-						<ProgressBar id="pg1" color="primary" progress={80} />
-						<p>Django</p>
-						<ProgressBar id="pg2" color="primary" progress={70} />
-						<p>Firebase</p>
-						<ProgressBar id="pg3" color="primary" progress={60} />
-						<p>React Native (TS/JS)</p>
-						<ProgressBar id="pg4" color="primary" progress={70} />
+						{WEB_SKILLS.map((skill, index) => (
+							<span key={index}>
+								<p>{skill.name}</p>
+								<ProgressBar
+									id={skill.id}
+									color={skill.color}
+									progress={skill.progress}
+								/>
+							</span>
+						))}
 					</div>
 					<div className="medium-divider"></div>
 					<div className="skills">
-							<Ball
-								color="var(--tertiary-container)"
-								top="10vh"
-								left="2vw"
-								size="60px"
-							/>
-							<Square
-								left="30vw"
-								top="25vh"
-								radius="10px"
-								color="var(--inverse-surface)"
-							/>
-							<Ball
-								color="var(--primary)"
-								top="2px"
-								left="60vw"
-								size="50px"
-							/>
+						<Ball
+							color="var(--tertiary-container)"
+							top="10vh"
+							left="2vw"
+							size="60px"
+						/>
+						<Square
+							left="30vw"
+							top="25vh"
+							radius="10px"
+							color="var(--inverse-surface)"
+						/>
+						<Ball color="var(--primary)" top="2px" left="60vw" size="50px" />
 						<h4 className="skills-head">
 							{"<>"}Programming Languages{"</>"}
 						</h4>
-						<p>TypeScript</p>
-						<ProgressBar id="pg5" color="secondary" progress={80} />
-						<p>JavaScript</p>
-						<ProgressBar id="pg6" color="secondary" progress={80} />
-						<p>Python</p>
-						<ProgressBar id="pg7" color="secondary" progress={60} />
-						<p>C</p>
-						<ProgressBar id="pg8" color="secondary" progress={60} />
-						<p>Java</p>
-						<ProgressBar id="pg9" color="secondary" progress={70} />
-						<p>HTML5</p>
-						<ProgressBar id="pg10" color="secondary" progress={95} />
-						<p>CSS3</p>
-						<ProgressBar id="pg11" color="secondary" progress={90} />
+						{PROGRAMMING_LANGS.map((skill, index) => (
+							<span key={index}>
+								<p>{skill.name}</p>
+								<ProgressBar
+									id={skill.id}
+									color={skill.color}
+									progress={skill.progress}
+								/>
+							</span>
+						))}
 					</div>
 					<div className="medium-divider"></div>
 					<div className="skills">
-							<Square
-								left="2vw"
-								top="20vh"
-								radius="10px"
-								color="var(--primary-container)"
-							/>
-							<Ball color="var(--error)" top="10vh" left="30vw" size="60px" />
-							<Square
-								left="65vw"
-								top="30vh"
-								radius="10px"
-								color="var(--tertiary-container)"
-							/>
+						<Square
+							left="2vw"
+							top="20vh"
+							radius="10px"
+							color="var(--primary-container)"
+						/>
+						<Ball color="var(--error)" top="10vh" left="30vw" size="60px" />
+						<Square
+							left="65vw"
+							top="30vh"
+							radius="10px"
+							color="var(--tertiary-container)"
+						/>
 						<h4 className="skills-head">
 							{"<>"}Technologies Known{"</>"}
 						</h4>
-						<p>Git and GitHub</p>
-						<ProgressBar id="pg17" color="tertiary" progress={85} />
-						<p>Material UI</p>
-						<ProgressBar id="pg13" color="tertiary" progress={85} />
-						<p>Material You (Material Design 3)</p>
-						<ProgressBar id="pg14" color="tertiary" progress={75} />
-						<p>Operating System</p>
-						<ProgressBar id="pg15" color="tertiary" progress={60} />
-						<p>Unix and Linux</p>
-						<ProgressBar id="pg16" color="tertiary" progress={60} />
+						{TECH_KNOWN.map((skill, index) => (
+							<span key={index}>
+								<p>{skill.name}</p>
+								<ProgressBar
+									id={skill.id}
+									color={skill.color}
+									progress={skill.progress}
+								/>
+							</span>
+						))}
 					</div>
 				</div>
 			</div>
